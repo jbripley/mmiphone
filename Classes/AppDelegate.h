307087d1ab95184e781d81d75212a2ba1f9fb7b5
Joakim Bodin <joakim.bodin@gmail.com>
2010-05-08 13:07:44 +0200

    Initial commit

---
+++ Classes/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  mmiphoneAppDelegate.h
+//  mmiphone
+//
+//  Created by Joakim Bodin on 2010-05-08.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+  NSManagedObjectModel*         _managedObjectModel;
+  NSManagedObjectContext*       _managedObjectContext;
+  NSPersistentStoreCoordinator* _persistentStoreCoordinator;
+
+  // App State
+  BOOL                          _modelCreated;
+  BOOL                          _resetModel;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext* managedObjectContext;
+@property (nonatomic, readonly)         NSString*               applicationDocumentsDirectory;
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  mmiphone
+//
+//  Created by Joakim Bodin on 2010-05-08.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+int main(int argc, char *argv[]) {
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+  [pool release];
+  return retVal;
+}
