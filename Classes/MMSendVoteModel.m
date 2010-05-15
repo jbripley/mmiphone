27888fdbccc6c74be9e168bd7e65f71dc58da69a
Joakim Bodin <joakim.bodin@gmail.com>
2010-05-15 15:07:19 +0200

    Make sure to send vote only once

--- Classes/MMSendVoteModel.m
+++ Classes/MMSendVoteModel.m
@@ -38,25 +38,27 @@ static NSString* kMMVoteFormat = @"%@/vote";
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)load:(TTURLRequestCachePolicy)cachePolicy more:(BOOL)more {
-  NSString* serverURL = [[NSUserDefaults standardUserDefaults]
-                         stringForKey:@"serverURL_preference"];
-  NSString* url = [NSString stringWithFormat:kMMVoteFormat, serverURL];
-  
-  TTURLRequest* request = [TTURLRequest requestWithURL:url delegate: self];
-  request.httpMethod = @"POST";
-  request.cachePolicy = cachePolicy;
-  request.response = [[[TTURLDataResponse alloc] init] autorelease];
-  
-  NSMutableDictionary* voteParams = [[NSMutableDictionary alloc] init];
-  [voteParams setObject:self.trackUri forKey:@"track"];
-  [voteParams setObject:[MMUser userId] forKey:@"user"];
-  NSString* voteJSON = [voteParams yajl_JSONString];
-  TT_RELEASE_SAFELY(voteParams);
-  
-  request.httpBody = [voteJSON dataUsingEncoding:NSUTF8StringEncoding];
-  TTDINFO(@"Send vote request URL: %@", [request URL]);
-  
-  [request send];
+  if (!self.isLoading) {
+    NSString* serverURL = [[NSUserDefaults standardUserDefaults]
+                           stringForKey:@"serverURL_preference"];
+    NSString* url = [NSString stringWithFormat:kMMVoteFormat, serverURL];
+    
+    TTURLRequest* request = [TTURLRequest requestWithURL:url delegate: self];
+    request.httpMethod = @"POST";
+    request.cachePolicy = cachePolicy;
+    request.response = [[[TTURLDataResponse alloc] init] autorelease];
+    
+    NSMutableDictionary* voteParams = [[NSMutableDictionary alloc] init];
+    [voteParams setObject:self.trackUri forKey:@"track"];
+    [voteParams setObject:[MMUser userId] forKey:@"user"];
+    NSString* voteJSON = [voteParams yajl_JSONString];
+    TT_RELEASE_SAFELY(voteParams);
+    
+    request.httpBody = [voteJSON dataUsingEncoding:NSUTF8StringEncoding];
+    TTDINFO(@"Send vote request URL: %@", [request URL]);
+    
+    [request send];
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
