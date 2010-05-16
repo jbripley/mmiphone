65d74364e61d583240bb24ca86c905328e0326ba
Joakim Bodin <joakim.bodin@gmail.com>
2010-05-16 16:05:50 +0200

    Add table item for detailed track info

---
+++ Classes/MMTableTrackDetailItemCell.m
@@ -0,0 +1,49 @@
+//
+// Copyright 2010 Joakim Bodin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import "MMTableTrackDetailItemCell.h"
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+@implementation MMTableTrackDetailItemCell
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString*)identifier {
+  if (self = [super initWithStyle:style reuseIdentifier:identifier]) {
+    self.textLabel.lineBreakMode = UILineBreakModeTailTruncation;
+  }
+  
+  return self;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)setObject:(id)object {
+  if (_item != object) {
+    [super setObject:object];
+    self.textLabel.font = TTSTYLEVAR(font);
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
++ (UIFont*)textFontForItem:(TTTableTextItem*)item {
+  return TTSTYLEVAR(font);
+}
+
+@end
+
